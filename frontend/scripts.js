0d9770731c2c192ed70bb433296cffa3fa79fea4
Zach North <znorth@riotgames.com>
2017-06-12 22:45:26 -0700

    updated frontend to flip cards and increment the index, and added basic express stuff for future data layer

---
+++ frontend/scripts.js
@@ -0,0 +1,35 @@
+var CURRENT_CARD_INDEX = 0;
+
+var initEventHandlers = function() {
+	var cardFront = document.getElementById('card-front');
+	var cardBack = document.getElementById('card-back');
+
+	// By default, the cardBack is not displayed and the cardFront is.
+	cardFront.style.display = '';
+	cardBack.style.display = 'none';
+
+	cardFront.addEventListener('click', function(e) {
+		console.log('clicked front, flipping card...');
+		cardFront.style.display = 'none';
+		cardBack.style.display = '';
+	});
+
+	cardBack.addEventListener('click', function(e) {
+		console.log('clicked back, going to next card');
+		// for now just flip back
+		loadNextCard();
+		cardFront.style.display = '';
+		cardBack.style.display = 'none';
+	});
+};
+
+var loadNextCard = function() {
+	// do some work to get the next card. update cardFront and cardBack
+	CURRENT_CARD_INDEX++;
+	document.getElementById('card-index').innerHTML = CURRENT_CARD_INDEX;
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+	loadNextCard();
+	initEventHandlers();
+});
