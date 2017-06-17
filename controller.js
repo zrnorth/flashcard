f00130bd80130d53b367622ede25f51c0d631827
Zach North <znorth@riotgames.com>
2017-06-17 15:25:31 -0700

    changed to return card id instaed of data object

--- controller.js
+++ controller.js
@@ -1,10 +1,16 @@
 const db = require('./postgres.js');
 
 exports.newCard = function(front, back) {
-	// find the difficulty rating of the new card and then add it to the db. return succ/fail
+	return db.addCard(front, back, 2.5).then(function(data) {
+		return data.id;
+	});
 }
 
 exports.logReview = function(id, response_quality) {
 	// get the card's easiness factor and modify it based on the response quality.
 	// calculate the next review interval and save it to the db with reps / next review date
+}
+
+exports.getTodaysCards = function() {
+	return db.getTodaysCards();
 }
