d8c44c9cd749b0610aca9eebefb3f94b43cfe52a
Zach North <znorth@riotgames.com>
2017-06-19 11:47:19 -0700

    added getUpdatedDifficulty

--- SM2.js
+++ SM2.js
@@ -1,8 +1,17 @@
 // SM2 is explained here: https://www.supermemo.com/english/ol/sm2.htm
+
 exports.getUpdatedDifficulty = function(oldDifficulty, responseQuality) {
-	// Given the old difficulty rating and a new response with a quality 0-5,
-	// calculate the new difficulty rating for the card
-	return 2.1;
+	var newDifficulty =  (oldDifficulty - 0.8) + 
+						 (0.28 * responseQuality) - 
+						 (0.02 * responseQuality * responseQuality);
+	// difficulty has to be in the range [1.3, 2.5].
+	if (newDifficulty < 1.3) {
+		newDifficulty = 1.3;
+	}
+	else if (newDifficulty > 2.5) {
+		newDifficulty = 2.5;
+	}
+	return round(newDifficulty, 4);
 }
 
 exports.getDaysUntilNextReview = function(reps, difficulty) {
@@ -18,7 +27,7 @@ exports.getDaysUntilNextReview = function(reps, difficulty) {
 	if (difficulty < 1.3 || difficulty > 2.5) {
 		throw 'Difficulty rating must be between [1.3, 2.5]';
 	}
-	
+
 	switch(reps) {
 		case 0:
 			return 0;
@@ -29,4 +38,9 @@ exports.getDaysUntilNextReview = function(reps, difficulty) {
 		default: // reps > 2
 			return Math.ceil(this.getDaysUntilNextReview((reps - 1), difficulty) * difficulty);
 	}
+}
+
+// helper to round a decimal: http://www.jacklmoore.com/notes/rounding-in-javascript/
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
--- test/SM2_test.js
+++ test/SM2_test.js
@@ -1,5 +1,20 @@
 var SM2 = require('../SM2.js')
 var run = 1;
+
+/* getUpdatedDifficulty test
+for (var diff = 1.3; diff <= 2.5; diff += 0.1) {
+	for (var qual = 0; qual <= 5; qual++) {
+		console.log('run: ' + run);
+		run++;
+		console.log('old diff: ', diff);
+		console.log('resp.quality: ', qual);
+		console.log('new diff: ', SM2.getUpdatedDifficulty(diff, qual));
+		console.log('---');
+	}
+}
+*/
+
+/* getDaysUntilNextReview test
 for (var diff = 1.0; diff < 2.7; diff += 0.2) {
 	for (var reps = 0; reps < 20; reps++) {
 		console.log('run: ', run);
@@ -15,3 +30,4 @@ for (var diff = 1.0; diff < 2.7; diff += 0.2) {
 		}
 	}
 }
+*/
