f30a86065b9f814836c4bb07e3f394a492926c95
Zach North <znorth@riotgames.com>
2017-07-24 17:24:04 -0700

    Modified the way updateCard works so you can change any field, not just the ones needed for reviews. Also fixed indentation

--- data/dataController.js
+++ data/dataController.js
@@ -4,83 +4,83 @@ require('../helpers/dateHelpers.js');
 
 
 exports.newCard = function(front, back, userId) {
-    return db.addCard(front, back, 2.5, userId)
-    .then(function(data) {
-        return data.id;
-    });
+  return db.addCard(front, back, 2.5, userId)
+  .then(function(data) {
+    return data.id;
+  });
 }
 
 exports.newCards = function(cards, userId) {
-    return db.addCards(cards, userId)
-    .then(function(data) {
-        var ids = [];
-        data.forEach(function(item) {
-            ids.push(item.id);
-        });
-        return ids;
-    })
-    .catch(function(error) {
-        if (error.message && error.message.includes('unique constraint')) {
-            throw error.first.detail;
-        }
-        else {
-            throw error;
-        }
+  return db.addCards(cards, userId)
+  .then(function(data) {
+    var ids = [];
+    data.forEach(function(item) {
+      ids.push(item.id);
     });
+    return ids;
+  })
+  .catch(function(error) {
+    if (error.message && error.message.includes('unique constraint')) {
+      throw error.first.detail;
+    }
+    else {
+      throw error;
+    }
+  });
 }
 
 exports.deleteCard = function(id) {
-    return db.deleteCard(id);
+  return db.deleteCard(id);
 }
 
 exports.deleteCardByFront = function(front) {
-    return db.getCardByFront(front).then(function(cards) {
-        if (cards.length > 0) { // just delete the first one you find for now
-            return db.deleteCard(cards[0].id);
-        }
-    });
+  return db.getCardByFront(front).then(function(cards) {
+    if (cards.length > 0) { // just delete the first one you find for now
+      return db.deleteCard(cards[0].id);
+    }
+  });
 }
 
 exports.deleteAllForUser = function(userId) {
-    return db.deleteAllForUser(userId);
+  return db.deleteAllForUser(userId);
 }
 
 exports.deleteAll = function() {
-    return db.deleteAll();
+  return db.deleteAll();
 }
 
 exports.logReview = function(id, responseQuality) {
-    return db.getCard(id).then(function(card) {
-        // If the review was correct, update the card's correct reps and difficulty in the db.
-        // Return the date the card should next be reviewed.
-        if (responseQuality >= 3) {
-            var difficulty = SM2.getUpdatedDifficulty(card.difficulty, responseQuality);
-            var today = Date.simpleToday();
-            var nextReviewDate = today.addDays(SM2.getDaysUntilNextReview(card.reps, difficulty));
+  return db.getCard(id).then(function(card) {
+    // If the review was correct, update the card's correct reps and difficulty in the db.
+    // Return the date the card should next be reviewed.
+    if (responseQuality >= 3) {
+      var difficulty = SM2.getUpdatedDifficulty(card.difficulty, responseQuality);
+      var today = Date.simpleToday();
+      var nextReviewDate = today.addDays(SM2.getDaysUntilNextReview(card.reps, difficulty));
 
-            return db.updateCard(id, difficulty, nextReviewDate, card.reps + 1).then(function(updatedCard) {
-                return updatedCard.next_review;
-            });
-        }
-        // If it was incorrect, reset the number of reps, leaving the difficulty the same.
-        else {
-            return db.resetForgottenCard(id).then(function(updatedCard) {
-                return updatedCard.next_review;
-            });
-        }
-    });
+      return db.updateCard(id, card.front, card.back, nextReviewDate, difficulty, card.reps + 1).then(function(updatedCard) {
+        return updatedCard.next_review;
+      });
+    }
+    // If it was incorrect, reset the number of reps, leaving the difficulty the same.
+    else {
+      return db.resetForgottenCard(id).then(function(updatedCard) {
+        return updatedCard.next_review;
+      });
+    }
+  });
 }
 
 exports.getTodaysCards = function(userId) {
-    return db.getTodaysCards(userId);
+  return db.getTodaysCards(userId);
 }
 
 exports.getAllCardsForUser = function(userId, limit, offset, orderBy) {
-    return db.getAllCardsForUser(userId, limit, offset, orderBy);
+  return db.getAllCardsForUser(userId, limit, offset, orderBy);
 }
 
 exports.getTotalNumberOfCards = function(userId) {
-    return db.getTotalNumberOfCards(userId).then(function(data) {
-        return data.count;
-    });
+  return db.getTotalNumberOfCards(userId).then(function(data) {
+    return data.count;
+  });
 }
--- data/postgres.js
+++ data/postgres.js
@@ -4,37 +4,37 @@ require('../helpers/dateHelpers.js');
 // Set bluebird as the default promise library for pgp.
 const promise = require('bluebird');
 const pgp = require('pg-promise')({
-    promiseLib: promise
+  promiseLib: promise
 });
 
 var databaseUrl;
 switch (process.env.NODE_ENV) {
-    case 'DEV':
-    case 'DEVELOPMENT': {
-        pgp.pg.defaults.ssl = false;
-        // Change this to your local postgres information.
-        databaseUrl = 'postgres://znorth:@localhost:5432/test';
-        break;
-    }
+  case 'DEV':
+  case 'DEVELOPMENT': {
+    pgp.pg.defaults.ssl = false;
+    // Change this to your local postgres information.
+    databaseUrl = 'postgres://znorth:@localhost:5432/test';
+    break;
+  }
 
-    case 'PROD':
-    case 'PRODUCTION': {
-        pgp.pg.defaults.ssl = true;
-        databaseUrl = process.env.DATABASE_URL;
-        if (!databaseUrl) {
-            console.error('You are trying to run against the production db, but haven\'t set up the db url through heroku yet.');
-            console.error('You can run this:');
-            console.error('export DATABASE_URL=$(heroku config:get DATABASE_URL -a zrnorth-flashcards)');
-            console.error('to set it.');
-        }
-        break;
+  case 'PROD':
+  case 'PRODUCTION': {
+    pgp.pg.defaults.ssl = true;
+    databaseUrl = process.env.DATABASE_URL;
+    if (!databaseUrl) {
+      console.error('You are trying to run against the production db, but haven\'t set up the db url through heroku yet.');
+      console.error('You can run this:');
+      console.error('export DATABASE_URL=$(heroku config:get DATABASE_URL -a zrnorth-flashcards)');
+      console.error('to set it.');
     }
-    default: {
-        console.error('You need to set NODE_ENV before starting this. Run');
-        console.error('export NODE_ENV=DEV | PROD');
-        console.error('to choose which db to run against.')
-        return;
-    }
+    break;
+  }
+  default: {
+    console.error('You need to set NODE_ENV before starting this. Run');
+    console.error('export NODE_ENV=DEV | PROD');
+    console.error('to choose which db to run against.')
+    return;
+  }
 }
 
 // Setup the connection to postgres
@@ -42,25 +42,25 @@ const postgres = pgp(databaseUrl);
 
 // --- USER operations
 exports.createUser = function(username, hashedPassword) {
-    return postgres.one('INSERT INTO users (USERNAME, PASSWORD) values ($1, $2) RETURNING ID', [username, hashedPassword])
-        .finally(pgp.end());
+  return postgres.one('INSERT INTO users (USERNAME, PASSWORD) values ($1, $2) RETURNING ID', [username, hashedPassword])
+    .finally(pgp.end());
 }
 
 exports.getUserByUsername = function(username) {
-    return postgres.one('SELECT * FROM users WHERE USERNAME=$1', [username])
-        .finally(pgp.end());
+  return postgres.one('SELECT * FROM users WHERE USERNAME=$1', [username])
+    .finally(pgp.end());
 }
 
 exports.deleteUser = function(id) {
-    return postgres.none('DELETE FROM users where ID=$1', [id])
-        .finally(pgp.end());
+  return postgres.none('DELETE FROM users where ID=$1', [id])
+    .finally(pgp.end());
 }
 
 /*
 create table users(
-    ID              SERIAL PRIMARY KEY          NOT NULL,
-    USERNAME        VARCHAR(200)                NOT NULL UNIQUE,
-    PASSWORD        VARCHAR(100)                NOT NULL
+  ID        SERIAL PRIMARY KEY      NOT NULL,
+  USERNAME    VARCHAR(200)        NOT NULL UNIQUE,
+  PASSWORD    VARCHAR(100)        NOT NULL
 );
 */
 
@@ -71,84 +71,85 @@ create table users(
 // the string formatting is funky here, but the $1 $2 method is hard to do with limit / offset and
 // i got tired of looking for the clean way to do it :)
 exports.getAllCardsForUser = function(userId, limit, offset, orderBy='ID') {
-    var sql = 'SELECT ROW_NUMBER() OVER (ORDER BY ' + orderBy + '), * FROM cards WHERE OWNER_ID=' + userId + ' ORDER BY ' + orderBy;
-    if (limit !== undefined && offset !== undefined) {
-        sql += ' LIMIT ' + limit + ' OFFSET ' + offset;
-    }
-    return postgres.any(sql)
-        .finally(pgp.end());
+  var sql = 'SELECT ROW_NUMBER() OVER (ORDER BY ' + orderBy + '), * FROM cards WHERE OWNER_ID=' + userId + ' ORDER BY ' + orderBy;
+  if (limit !== undefined && offset !== undefined) {
+    sql += ' LIMIT ' + limit + ' OFFSET ' + offset;
+  }
+  return postgres.any(sql)
+    .finally(pgp.end());
 }
 
 exports.getTotalNumberOfCards = function(userId) {
-    return postgres.one('SELECT count(*) AS count FROM cards WHERE OWNER_ID=$1', [userId])
-        .finally(pgp.end());
+  return postgres.one('SELECT count(*) AS count FROM cards WHERE OWNER_ID=$1', [userId])
+    .finally(pgp.end());
 }
 
 exports.getTodaysCards = function(userId) {
-    return postgres.any('SELECT * FROM cards WHERE OWNER_ID=$1 AND NEXT_REVIEW <= CURRENT_DATE ORDER BY RANDOM()', [userId])
-        .finally(pgp.end());
+  return postgres.any('SELECT * FROM cards WHERE OWNER_ID=$1 AND NEXT_REVIEW <= CURRENT_DATE ORDER BY RANDOM()', [userId])
+    .finally(pgp.end());
 };
 
 exports.addCard = function(front, back, difficulty, userId) {
-    return postgres.one('INSERT INTO cards(FRONT, BACK, NEXT_REVIEW, DIFFICULTY, REPS, OWNER_ID) VALUES($1, $2, $3, $4, 0, $5) RETURNING ID', 
-      [front, back, Date.simpleToday(), difficulty, userId])
-        .finally(pgp.end());
+  return postgres.one('INSERT INTO cards(FRONT, BACK, NEXT_REVIEW, DIFFICULTY, REPS, OWNER_ID) VALUES($1, $2, $3, $4, 0, $5) RETURNING ID', 
+    [front, back, Date.simpleToday(), difficulty, userId])
+    .finally(pgp.end());
 };
 
 exports.addCards = function(cards, userId) {
-    return postgres.tx(function(t) {
-        var queries = []
-        cards.forEach(function(card) {
-            var q = t.one('INSERT INTO cards(FRONT, BACK, NEXT_REVIEW, DIFFICULTY, REPS, OWNER_ID) VALUES ($1, $2, CURRENT_DATE, 2.5, 0, $3) RETURNING ID',
-                [card.front, card.back, userId]); // No custom difficulty in bulk adding! Too annoying to do
-            queries.push(q);
-        });
-        return t.batch(queries);
-    }).finally(pgp.end());
+  return postgres.tx(function(t) {
+    var queries = []
+    cards.forEach(function(card) {
+      var q = t.one('INSERT INTO cards(FRONT, BACK, NEXT_REVIEW, DIFFICULTY, REPS, OWNER_ID) VALUES ($1, $2, CURRENT_DATE, 2.5, 0, $3) RETURNING ID',
+        [card.front, card.back, userId]); // No custom difficulty in bulk adding! Too annoying to do
+      queries.push(q);
+    });
+    return t.batch(queries);
+  }).finally(pgp.end());
 }
 
 exports.getCard = function(id) {
-    return postgres.one('SELECT * FROM cards WHERE ID=$1', [id])
-        .finally(pgp.end());
+  return postgres.one('SELECT * FROM cards WHERE ID=$1', [id])
+    .finally(pgp.end());
 };
 
 exports.getCardByFront = function(front) {
-    return postgres.any('SELECT * FROM cards where FRONT=$1', [front])
-        .finally(pgp.end());
+  return postgres.any('SELECT * FROM cards where FRONT=$1', [front])
+    .finally(pgp.end());
 }
 
-exports.updateCard = function(id, difficulty, review_date, reps) {
-    return postgres.one('UPDATE cards SET DIFFICULTY=$1, NEXT_REVIEW=$2, REPS=$3 WHERE ID=$4 RETURNING NEXT_REVIEW', [difficulty, review_date, reps, id])
-        .finally(pgp.end());
+exports.updateCard = function(id, front, back, next_review, difficulty, reps) {
+  return postgres.one('UPDATE cards SET FRONT=$1, BACK=$2, NEXT_REVIEW=$3, DIFFICULTY=$4, REPS=$5 WHERE ID=$6 RETURNING NEXT_REVIEW',
+    [front, back, next_review, difficulty, reps, id])
+    .finally(pgp.end());
 };
 
 exports.resetForgottenCard = function(id) {
-    return postgres.one('UPDATE cards SET REPS=0, NEXT_REVIEW = CURRENT_DATE where ID=$1 RETURNING NEXT_REVIEW', [id])
-        .finally(pgp.end());
+  return postgres.one('UPDATE cards SET REPS=0, NEXT_REVIEW = CURRENT_DATE where ID=$1 RETURNING NEXT_REVIEW', [id])
+    .finally(pgp.end());
 };
 
 exports.deleteCard = function(id) {
-    return postgres.none('DELETE FROM cards WHERE ID=$1', [id])
-        .finally(pgp.end());
+  return postgres.none('DELETE FROM cards WHERE ID=$1', [id])
+    .finally(pgp.end());
 };
 
 exports.deleteAll = function() {
-    return postgres.none('DELETE FROM cards WHERE ID>0')
-        .finally(pgp.end());
+  return postgres.none('DELETE FROM cards WHERE ID>0')
+    .finally(pgp.end());
 }
 
 exports.deleteAllForUser = function(userId) {
-    return postgres.none('DELETE FROM cards WHERE OWNER_ID=$1', userId)
-        .finally(pgp.end());
+  return postgres.none('DELETE FROM cards WHERE OWNER_ID=$1', userId)
+    .finally(pgp.end());
 }
 /*
 create table cards(
-    ID              SERIAL PRIMARY KEY          NOT NULL,
-    FRONT           TEXT                        NOT NULL,
-    BACK            TEXT                        NOT NULL,
-    NEXT_REVIEW     DATE                        NOT NULL,
-    DIFFICULTY      REAL                        NOT NULL,
-    REPS            INT                         NOT NULL,
-    OWNER_ID        INT REFERENCES users(ID)    ON DELETE CASCADE NOT NULL,
-    UNIQUE(FRONT, BACK)
+  ID        SERIAL PRIMARY KEY      NOT NULL,
+  FRONT       TEXT            NOT NULL,
+  BACK      TEXT            NOT NULL,
+  NEXT_REVIEW   DATE            NOT NULL,
+  DIFFICULTY    REAL            NOT NULL,
+  REPS      INT             NOT NULL,
+  OWNER_ID    INT REFERENCES users(ID)  ON DELETE CASCADE NOT NULL,
+  UNIQUE(FRONT, BACK)
 ); */
