7b9473a9a1da1fd146a394846046126fc6472c56
Zach North <znorth@riotgames.com>
2017-08-05 22:37:53 -0700

    Default to kana on input, and fix the plus minus buttons weirdness

--- public/js/create.js
+++ public/js/create.js
@@ -1,25 +1,25 @@
 // Depends on /lib/wanakana.min.js
 $(function() {
-  $(document).on('click', '.btn-add', function(e) {
+  $(document).on('click', '.btn-success', function(e) {
     e.preventDefault();
     // Copy the current row
-    var firstRow = $('.row:first');
-    var newRow = $(firstRow.clone()).insertBefore('form .row:last');
+    var thisRow = $(this).parents('.row');
+    var newRow = $(thisRow.clone()).insertAfter(thisRow);
     // Remove any previously entered text from the new row
     newRow.find('input').val('');
     // Replace the + button with a - button
-    $('form').find('.row:not(:last) .btn-add')
-      .removeClass('btn-add').addClass('btn-remove')
+    $('form').find('.row:not(:last) .btn-success')
       .removeClass('btn-success').addClass('btn-danger')
       .html('<span class="glyphicon glyphicon-minus"></span>');
     updateKanaInput();
-  }).on('click', '.btn-remove', function(e) {
+  }).on('click', '.btn-danger', function(e) {
     e.preventDefault();
-    $(this).parents('.row:first').remove();
+    $(this).parents('.row').remove();
     return false;
   }).on('click', '#input-format-form', function(e) {
     updateKanaInput();
   });
+  updateKanaInput();
 });
 
 var updateKanaInput = function() {
