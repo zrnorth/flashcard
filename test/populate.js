cd0e3b5ff5c53ca4c29d8ceca41092040a2f21df
Zach North <znorth@riotgames.com>
2017-08-11 16:30:23 -0700

    Changed populate script to make cards with random length

--- test/populate.js
+++ test/populate.js
@@ -1,5 +1,11 @@
 var dataController = require('../data/dataController.js')
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
 const userId = process.env.npm_config_userId || process.env.USER_ID || null;
 if (!userId) {
     console.error('Need a user id to add the cards to. Use --userId=USER_ID');
@@ -10,12 +16,12 @@ const numCards = process.env.npm_config_numCards || 3;
 
 var cards = [];
 for (var i = 0; i < numCards; i++) {
-    cards.push({
-        front: Math.random().toString(36).substring(2, 7),
-        back: Math.random().toString(36).substring(2, 7)
-    });
+  cards.push({
+      front: Math.random().toString(36).substring(2, getRandomInt(7, 30)),
+      back: Math.random().toString(36).substring(2, getRandomInt(7, 30))
+  });
 }
 
 dataController.newCards(cards, parseInt(userId)).then(function(newCardIds) {
-  console.log('Added ' + numCards + ' new cards with ids ' + newCardIds);
+  console.log('Added ' + numCards + ' randomized new cards with ids ' + newCardIds);
 });
